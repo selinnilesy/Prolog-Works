f94820ecdf3590fda69cd3b9dc71c381249cbf0c
Selin Yıldırım <38113970+selinnilesy@users.noreply.github.com>
2020-05-31 16:50:56 +0300

    Update examples.pl
    Credit: Prolog exercises are taken from https://www.ic.unicamp.br/~meidanis/courses/mc336/2009s2/prolog/problemas/

--- examples.pl
+++ examples.pl
@@ -9,3 +9,11 @@ my_last(X,[H|T]) :- my_last(X,T).
 % Find the K'th element of a list, the first element in the list is number 1.
 element_at(Answer,[Answer|_],1). 
 element_at(Answer,[Head|Tail],N) :- New_N is N-1, element_at(Answer,Tail,New_N).
+
+% Find the number of elements of a list
+how_many(Answer,[],Answer).
+how_many(Answer,[Head|Tail],N) :- New_N is N+1, how_many(Answer,Tail,New_N).
+
+% Reverse a list
+my_reverse(Temp_list,[],Temp_list).
+my_reverse(Answer,[Head|Tail],Temp_list) :- my_reverse(Answer,Tail,[Head|Temp_list]).
